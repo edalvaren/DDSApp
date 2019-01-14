d16a3bbb0ef01e2848ad9c3c121597198a3b5f55
Erick Alvarenga <Erick.Alvarenga@Intralox.com>
2019-01-14 12:45:14 -0600

    fdas

---
+++ DDSApp/ClientApp/src/components/styled/StyledTitle.js
@@ -0,0 +1,17 @@
+﻿import React, {Component} from 'react';
+import styled from 'styled-components';
+
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: paletvioletred;
+    `;
+
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`;
+
+module.exports.Wrapper = Wrapper;
+module.exports.Title = Title;
